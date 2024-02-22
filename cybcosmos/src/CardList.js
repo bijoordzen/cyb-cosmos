2201cf72b660d378792bb93d5ba6a58aa9e7d6b3
unknown <bijoordzen@gmail.com>
2024-02-22 13:04:55 +0530

    update final

--- cybcosmos/src/CardList.js
+++ cybcosmos/src/CardList.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Card from './components/Card/Card';
-
+import Card from "./Card";
 const CardList = ({robots}) => {
     const cardComponent = robots.map((user,i) => {
         return <Card id = {robots[i].id} name = {robots[i].name} email = {robots[i].email} />
